fc524dc6ad0ea2840eb6cfa5bfaaf918af338e13
tomawas455 <tomawas455@student.polsl.pl>
2022-04-05 16:48:03 +0200

    changed access guards checks

--- backend/src/access_guards.py
+++ backend/src/access_guards.py
@@ -16,10 +16,9 @@ def login_required(view):
     return wrapped_view
 
 
-def check_roles(role_names, is_list=False):
-    print(g.user.role.name, is_list, role_names, file=sys.stderr)
-    if g.user is None or (not is_list and g.user.role.name != role_names)\
-            or (is_list and g.user.role.name not in role_names):
+def check_roles(role_names):
+    print(g.user.role.name, file=sys.stderr)
+    if g.user is None or g.user.role.name not in role_names:
         raise Unauthorized(
             "You need to have different permissions to see this"
         )
@@ -28,7 +27,7 @@ def check_roles(role_names, is_list=False):
 def only_user(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        check_roles('user')
+        check_roles(['user'])
         return view(**kwargs)
 
     return wrapped_view
@@ -37,7 +36,7 @@ def only_user(view):
 def only_worker(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        check_roles('worker')
+        check_roles(['worker'])
         return view(**kwargs)
 
     return wrapped_view
@@ -46,7 +45,7 @@ def only_worker(view):
 def only_admin(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        check_roles('administrator')
+        check_roles(['administrator'])
         return view(**kwargs)
 
     return wrapped_view
@@ -55,7 +54,7 @@ def only_admin(view):
 def only_user_or_worker(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        check_roles(['user', 'worker'], True)
+        check_roles(['user', 'worker'])
         return view(**kwargs)
 
     return wrapped_view
