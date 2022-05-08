8ea964e62147fec751e0342ba78960430c5d9eb3
Kacpper <kacpro@onet.com.pl>
2022-05-08 20:09:43 +0200

    Add post and patch endpoint

--- backend/src/routes/participations.py
+++ backend/src/routes/participations.py
@@ -6,7 +6,9 @@ from werkzeug.exceptions import (
     BadRequest, NotFound
 )
 
-from access_guards import only_worker
+from datetime import datetime
+
+from access_guards import only_user, only_worker
 from models.courses import Course
 
 from models.db import db
@@ -31,3 +33,44 @@ def get_participants():
         "page_size": participations_page.per_page,
         "is_last": not participations_page.has_next
     }
+
+
+@bp.route('/create/<int:training_id>', methods=['POST'])
+@only_user
+def create_participants(training_id):
+    training = Training.query.filter_by(id=training_id).first()
+    if not training:
+        raise NotFound("Training with this id does not exist!")
+    data_json = request.get_json()
+    training.participants
+    if training.enrolment_begin_date > datetime.today():
+        raise BadRequest("You can not enroll on this training yet!")
+    if training.enrolment_end_date < datetime.today():
+        raise BadRequest("You can not enroll on this training anymore!")
+    if "users" in data_json and type(data_json["users"]) is list:
+        if training.free_places_amount < len(data_json["users"]):
+            raise BadRequest(f"There are not enough free places! Free places: {training.free_places_amount}")
+        participants = []
+        for user_id in data_json["users"]:
+            if Participation.query.filter_by(training_id=training_id, user_id=user_id).first() is not None:
+                raise BadRequest(f"User with id '{user_id}' has already enrolled on this training!")
+            participants.append(Participation(training_id, user_id))
+        db.session.add_all(participants)
+        training.free_places_amount -= len(participants)
+        db.session.commit()
+        return {"participants":[participant.to_dict() for participant in participants]}
+    raise BadRequest("Required parameter: 'users' list")
+
+
+@bp.route('/edit', methods=['PATCH'])
+@only_worker
+def edit_participation():
+    data_json = request.get_json()
+    if any(parameter not in data_json for parameter in ("user_id", "training_id", "passed")):
+        raise BadRequest("Required parameters: user_id, training_id")
+    participation = Participation.query.filter_by(user_id=data_json["user_id"], training_id=data_json["training_id"]).first()
+    if participation is None:
+        raise NotFound("Participation with this user id and training id does not exist!")
+    participation.passed = data_json["passed"]
+    db.session.commit()
+    return participation.to_dict()
