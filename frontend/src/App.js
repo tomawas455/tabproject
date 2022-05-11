86a2edc52852cc0fa4246586241d867ae85b6453
DawidFurs <dawid.furs@onet.pl>
2022-05-11 18:25:42 +0200

    Created login and registry view.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Header from './Header';
+import Login from './login';
+import Register from './register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+        <Header />
+        <Routes>
+             <Route exact path="/login" element={<Login />} />
+             <Route exact path="/register" element={<Register />} />
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/register.js
@@ -0,0 +1,44 @@
+import React ,{useState} from 'react'
+
+function Register()
+{
+    const [email,setEmail]=useState("")
+    const [name,setName]=useState("")
+    const [surname,setSurname]=useState("")
+    const [password,setPassword]=useState("")
+
+    async function signUp()
+    {
+    let item={email,name,surname,password}
+    console.warn(item)
+
+    let result= await fetch("http://localhost:8080/auth/register",{
+    method:"POST",
+    body: new URLSearchParams({
+            email: email,
+            name: name,
+            surname: surname,
+            password: password
+    })
+        })
+        result = await result.json()
+        console.warn("result", result)
+    }
+
+    return (
+            <div className="col-sm-4 offset-sm-4">
+            <h1> Register Page </h1>
+            <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" placeholder="email"/>
+            <br />
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" placeholder="name"/>
+            <br />
+            <input type="text" value={surname} onChange={(e)=>setSurname(e.target.value)} className="form-control" placeholder="surname"/>
+            <br />
+            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" placeholder="password"/>
+            <br />
+            <button onClick={signUp} className="btn btn-primary"> Sign Up </button>
+            </div>
+)
+}
+
+export default Register
